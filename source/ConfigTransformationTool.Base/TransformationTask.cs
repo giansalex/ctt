6b39adbb2e3abba29ddc1678e081b0ba687450fc
outcoldman <none@none.com>
2010-08-26 04:49:38 +0000

    Add first version.

---
+++ source/ConfigTransformationTool.Base/TransformationTask.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using Microsoft.Build.Utilities;
+using Microsoft.Web.Publishing.Tasks;
+
+namespace ConfigTransformationTool.Base
+{
+	/// <summary>
+	/// Make transformation of file <see cref="SourceFilePath"/> with transform file <see cref="TransformFile"/>.
+	/// Look at http://msdn.microsoft.com/en-us/library/dd465326.aspx for syntax of transformation file.
+	/// </summary>
+	public class TransformationTask
+	{
+		private readonly static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType); 
+
+		/// <summary>
+		/// Empty constructor
+		/// </summary>
+		public TransformationTask()
+		{
+		}
+
+		/// <summary>
+		/// Create new TransformationTask object and set values for <see cref="SourceFilePath"/> and <see cref="TransformFile"/>
+		/// </summary>
+		/// <param name="sourceFilePath">Source file path</param>
+		/// <param name="transformFilePath">Transformation file path</param>
+		public TransformationTask(string sourceFilePath, string transformFilePath)
+		{
+			Log.Debug("Create Transformation Task.");
+
+			SourceFilePath = sourceFilePath;
+			TransformFile = transformFilePath;
+		}
+
+		/// <summary>
+		/// Source file
+		/// </summary>
+		public string SourceFilePath { get; set; }
+
+		/// <summary>
+		/// Transformation file
+		/// </summary>
+		/// <remarks>
+		/// See http://msdn.microsoft.com/en-us/library/dd465326.aspx for syntax of transformation file
+		/// </remarks>
+		public string TransformFile { get; set; }
+
+		/// <summary>
+		/// Make transformation of file <see cref="SourceFilePath"/> with transform file <see cref="TransformFile"/> to <paramref name="destinationFilePath"/>.
+		/// </summary>
+		/// <param name="destinationFilePath">File path of destination transformation.</param>
+		/// <returns>Return true if transformation finish successfully, otherwise false.</returns>
+		public bool Execute(string destinationFilePath)
+		{
+			if (string.IsNullOrWhiteSpace(destinationFilePath))
+				throw new ArgumentException("Destination file can't be empty.", "destinationFilePath");
+
+			Log.DebugFormat("Start tranformation to '{0}'.", destinationFilePath);
+
+			if (string.IsNullOrWhiteSpace(SourceFilePath) || !File.Exists(SourceFilePath))
+				throw new FileNotFoundException("Can't find source file.", SourceFilePath);
+
+			if (string.IsNullOrWhiteSpace(TransformFile) || !File.Exists(TransformFile))
+				throw new FileNotFoundException("Can't find transform  file.", TransformFile);
+
+			Log.DebugFormat("Source file: '{0}'.", SourceFilePath);
+			Log.DebugFormat("Transform  file: '{0}'.", TransformFile);
+
+			try
+			{
+				TransformXml transformXml = new TransformXml
+				                            	{
+				                            		BuildEngine = new CurrentBuildEngine(),
+				                            		Destination = new TaskItem(destinationFilePath),
+													Source = new TaskItem(SourceFilePath),
+													Transform = new TaskItem(TransformFile)
+				                            	};
+				return transformXml.Execute();
+			} 
+			catch(Exception e)
+			{
+				Log.Error(e);
+				return false;
+			}
+		}
+	}
+}
---
+++ source/ConfigTransformationTool.Tests/BaseSpec.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Layout;
+using NUnit.Framework;
+
+namespace ConfigTransformationTool.Tests
+{
+	/// <summary>
+	/// Base test class. Configure log4net in <see cref="SetUp"/>.
+	/// </summary>
+	public abstract class BaseSpec
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			ConfigureLogger();
+		}
+
+		/// <summary>
+		/// Write <paramref name="content"/> to <paramref name="filePath"/>. If file <paramref name="filePath"/> exists it will be overwritten.
+		/// </summary>
+		/// <param name="filePath">File path</param>
+		/// <param name="content">Content of file</param>
+		protected void WriteToFile(string filePath, string content)
+		{
+			using (StreamWriter writer = new StreamWriter(filePath, false))
+			{
+				writer.Write(content);
+			}
+		}
+
+		/// <summary>
+		/// Configure log4net for showing trace information in output window.
+		/// </summary>
+		private static void ConfigureLogger()
+		{
+			PatternLayout patternLayout = new PatternLayout
+			                              	{
+			                              		ConversionPattern =
+			                              			"%message%newline"
+			                              	};
+			patternLayout.ActivateOptions();
+
+			ForwardingAppender appender = new ForwardingAppender();
+
+			TraceAppender traceAppender = new TraceAppender {Layout = patternLayout};
+			appender.AddAppender(traceAppender);
+
+			BasicConfigurator.Configure(appender);
+		}
+	}
+}
