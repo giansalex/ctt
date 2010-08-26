133182357c96c7c621ec3d8cb945b235dfb4c36c
outcoldman <none@none.com>
2010-08-26 04:49:38 +0000

    Add first version.

---
+++ source/ConfigTransformationTool.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Config Transformation Tool Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("outcoldman")]
+[assembly: AssemblyProduct("Config Transformation Tool Tests")]
+[assembly: AssemblyCopyright("Copyright © outcoldman 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2f553de-ccb1-4f3c-9b6f-93051e06257d")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
---
+++ source/ConfigTransformationTool.Tests/TransformationTaskSpec.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using ConfigTransformationTool.Base;
+using NUnit.Framework;
+
+namespace ConfigTransformationTool.Tests
+{
+	[TestFixture]
+	public class TransformationTaskSpec : BaseSpec
+	{
+		// ReSharper disable InconsistentNaming
+
+		[Test]
+		public void Transfarmotaion_Should_Happend()
+		{
+			string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+			string sourceFile = Path.Combine(baseDirectory, "Transfarmotaion_Should_Happend.config");
+			string transformFile =  Path.Combine(baseDirectory, "Transfarmotaion_Should_Happend_transform.config");
+			string resultFile =  Path.Combine(baseDirectory, "Transfarmotaion_Should_Happend_result.config");
+
+			// Create source file
+			WriteToFile(sourceFile, Source);
+
+			// Create transform file
+			WriteToFile(transformFile, Transform);
+
+			TransformationTask task = new TransformationTask(sourceFile, transformFile);
+			task.Execute(resultFile);
+
+			string fileContent;
+
+			using (StreamReader reader = new StreamReader(resultFile))
+			{
+				fileContent = reader.ReadToEnd();
+			}
+
+			//Check that transformation happend
+			Assert.IsTrue(fileContent.Contains(@"value=""601"""));
+		}
+
+		private const string Source = @"<?xml version=""1.0""?>
+
+<configuration>
+
+	<configSections>
+		<section name=""custom"" type=""MsiCommon.Configuration.CustomConfiguration, MsiCommon"" />
+	</configSections>
+
+	<custom>
+		<groups>
+			<group name=""TestGroup1"">
+				<values>
+					<value key=""Test1"" value=""True"" />
+					<value key=""Test2"" value=""600"" />
+				</values>
+			</group>
+
+			<group name=""TestGroup2"">
+				<values>
+					<value key=""Test3"" value=""True"" />
+				</values>
+			</group>
+
+		</groups>
+	</custom>
+	
+</configuration>";
+
+		private const string Transform = @"<?xml version=""1.0""?>
+<configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform"">
+	
+	<custom>
+		<groups>
+			<group name=""TestGroup1"">
+				<values>
+					<value key=""Test2"" value=""601"" xdt:Transform=""Replace""  xdt:Locator=""Match(key)"" />
+				</values>
+			</group>
+		</groups>
+	</custom>
+	
+</configuration>";
+
+		// ReSharper restore InconsistentNaming
+	}
+}
