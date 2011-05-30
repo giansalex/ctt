29f52badad7deaead273056d8def9c1e204d7701
outcoldman <none@none.com>
2011-05-30 22:23:32 +0000

    Add support to load parameters from file

---
+++ source/ConfigTransformationTool.Base/ParametersLoader.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace ConfigTransformationTool.Base
+{
+	public class ParametersLoader
+	{
+		public static void LoadParameters(string parametersFile, IDictionary<string, string> parameters)
+		{
+			if (string.IsNullOrWhiteSpace(parametersFile))
+				throw new ArgumentException("parametersFile");
+
+			if (!File.Exists(parametersFile))
+				throw new FileNotFoundException(string.Format("Couldn't find file with parameters '{0}'.", parametersFile));
+
+			XDocument document = XDocument.Load(parametersFile);
+			XElement root = document.Element("parameters");
+			
+			if (root == null)
+				throw new FormatException("Couldn't find root element 'parameters'.");
+
+			foreach(XElement element in root.Elements())
+			{
+				if (element.Name != "param")
+					throw new FormatException("All parameters elements should be 'param'.");
+
+				XAttribute attrName = element.Attribute("name");
+
+				if (attrName == null)
+					throw new FormatException("Attribute 'name' is required for 'param' element.");
+
+				XAttribute attrValue = element.Attribute("value");
+
+				if (attrValue == null)
+					throw new FormatException("Attribute 'value' is required for 'param' element.");
+
+				parameters.Add(attrName.Value, attrValue.Value);
+			}
+		}
+	}
+}
---
+++ source/ConfigTransformationTool.Tests/ParametersLoaderSpec.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ConfigTransformationTool.Base;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace ConfigTransformationTool.Tests
+{
+	[TestFixture]
+	public class ParametersLoaderSpec : BaseSpec
+	{
+		private const string ParametersFileContent = @"<?xml version=""1.0""?>
+<parameters>
+<param name=""ServerName"" value=""disneyland"" />
+<param name=""RootDirectory"" value=""D:\Disneyland"" />
+<param name=""DefaultUser"" value=""MickeyMouse"" />
+</parameters>";
+
+		[Test]
+		public void LoadParameters_FileWithParametes_ParametersShouldBeLoaded()
+		{
+			string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+			string parametersFile = Path.Combine(baseDirectory, "ParametersFile.xml");
+			WriteToFile(parametersFile, ParametersFileContent);
+
+			Dictionary<string,string> parameters = new Dictionary<string, string>();
+			ParametersLoader.LoadParameters(parametersFile, parameters);
+
+			Assert.AreEqual(3, parameters.Count);
+			Assert.AreEqual("disneyland", parameters["ServerName"]);
+			Assert.AreEqual("D:\\Disneyland", parameters["RootDirectory"]);
+			Assert.AreEqual("MickeyMouse", parameters["DefaultUser"]);
+		}
+	}
+}
